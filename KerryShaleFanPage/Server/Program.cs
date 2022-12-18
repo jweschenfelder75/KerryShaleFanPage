f09860d438a28194cb97951466f953312017c24f
byte2702 <weschi@gmx.com>
2022-12-18 14:33:28 +0100

    NLog added.

--- KerryShaleFanPage/Server/Program.cs
+++ KerryShaleFanPage/Server/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace KerryShaleFanPage
 {
@@ -8,17 +14,47 @@ namespace KerryShaleFanPage
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
 
-            // Add services to the container.
-            ConfigureServices(builder.Services);
+            var logger = NLogBuilder.ConfigureNLog(LogManager.Configuration).GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Start app");
+                var builder = WebApplication.CreateBuilder(args);
 
-            var app = builder.Build();
+                // Add services to the container.
+                ConfigureServices(builder.Services);
 
-            app = ConfigureApplication(app);
+                var app = builder.Build();
 
-            app.Run();
+                app = ConfigureApplication(app);
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped app because of exception");
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
+
+        /// <summary>
+        /// Creates the Host Buiilder
+        /// </summary>
+        /// <param name="args">Optional arguments from Main method</param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseNLog(new NLogAspNetCoreOptions()
+            {
+                LoggingConfigurationSectionName = "NLog"
+            });
 
         /// <summary>
         /// Configures the given ServiceCollection
