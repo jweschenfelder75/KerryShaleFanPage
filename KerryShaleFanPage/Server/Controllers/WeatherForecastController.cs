440c34ff52fc218ce82cd84ddf02df0aea925b21
byte2702 <weschenfelder@gmx.de>
2023-02-26 14:00:44 +0100

    Some formatting.

--- KerryShaleFanPage/Server/Controllers/WeatherForecastController.cs
+++ KerryShaleFanPage/Server/Controllers/WeatherForecastController.cs
@@ -13,8 +13,8 @@ namespace KerryShaleFanPage.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
